ade0fd834531b3e138e0b1eb705ba2b779303865
EdsonAmaya7 <edson_amaya99@live.com>
2023-02-01 22:11:12 -0600

    avances

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styles: [
+  ]
+})
+export class PorPaisComponent {
+  termino: string = ''
+  isError: boolean = false;
+  paises: Country[] = []
+  constructor(private paisService: PaisService) { }
+
+  buscar(termino: string) {
+    this.isError = false;
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino)
+      .subscribe({
+        next: (paises) => {
+          this.paises = paises;
+          this.isError = false
+        },
+        error: (err) => {
+          this.paises = [];
+          this.isError = true;
+        }
+      })
+  }
+
+}
